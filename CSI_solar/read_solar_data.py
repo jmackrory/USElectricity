69f025e5e7cb51a8daad0d5a545e6c038af02d5a
jmackrory <jonathan.mackrory@gmail.com>
2017-10-09 15:30:08 -0700

    made dummy function to join together series, with non-overlapping indices.

--- CSI_solar/read_solar_data.py
+++ CSI_solar/read_solar_data.py
@@ -99,31 +99,28 @@ def make_elec_series(df):
         ts_tot.append(ts)
     return ts_tot
 
-
-
 #Now make existing dataframe into a list of time series.
-def make_elec_series(df):
+def make_elec_dataframe(df):
     indx_vec=find_all_changes(df['ID'])
     num_series=len(indx_vec)
     name_vec=df.loc[indx_vec,'ID']
     name_vec.index = range(num_series)
     ts_tot=list();
-    #df_new=pd.DataFrame(columns=['ID','start','end','data'])
+    df_new=pd.DataFrame()
     tf='%d%b%y:%H:%M:%S'
     for i in range(num_series-1):
         name=name_vec[i]
-        times=pd.to_datetime(df.loc[indx_vec[i]:indx_vec[i+1]-1,'LocalDateTime'],format=tf)
-        # start_time=pd.to_datetime(df.loc[indx_vec[i],'LocalDateTime'],format=tf)
-        # end_time=pd.to_datetime(df.loc[indx_vec[i+1]-1,'LocalDateTime'],format=tf)
-        # # timeindex=pd.DatetimeIndex(start=start_time,end=end_time,freq='15 min')
+        #times=pd.to_datetime(df.loc[indx_vec[i]:indx_vec[i+1]-1,'LocalDateTime'],format=tf)
+        start_time=pd.to_datetime(df.loc[indx_vec[i],'LocalDateTime'],format=tf)
+        end_time=pd.to_datetime(df.loc[indx_vec[i+1]-1,'LocalDateTime'],format=tf)
+        times=pd.DatetimeIndex(start=start_time,end=end_time,freq='15 min')
         elec=df.loc[indx_vec[i]:indx_vec[i+1]-1,'kWh'].values
         ts = pd.Series(data=elec,index=times)
-        ts_tot.append(ts)
-    return ts_tot
+        df_new[name]=ts
+    return df_new
 
-
-df1= make_elec_series(df)
-
+#df1= make_elec_series(df)
+#Plot a list of timeseries
 def plot_data(date,df):
     for s in df:
         try:plt.plot(s[date])
@@ -131,6 +128,24 @@ def plot_data(date,df):
     plt.show()
     return
 
+def make_dummy_frame():
+    time=[pd.DatetimeIndex(['2012','2014','2015']),
+          pd.DatetimeIndex(['2011','2012','2013','2014']),
+          pd.DatetimeIndex(['2011','2012','2015','2016'])]
+    data=[[1,3.4,9],[1.0,0.0,-1.0,0.1],[2,3,5,7]]
+
+    df=pd.DataFrame()
+    names=['name1','name2','name3']
+    df=pd.DataFrame(data[0],index=time[0],columns=[names[0]])
+    for i in range(1,3):
+        df_loc=pd.DataFrame(data[i],index=time[i],columns=[names[i]])
+        print(i)
+        print(df_loc)
+        df=df.join(df_loc,how='outer')
+    #df.columns=names
+    return df
+
+dummyframe=make_dummy_frame()
 #When getting satellite data restrict from 5am-7pm.
 
 #Randomly sample 10% of days as test set.
@@ -142,7 +157,13 @@ def plot_data(date,df):
 #  * Could measure via average integrated power for a day, plot for whole dataset on yearly basis.
 #  * Take weekly chunks to find variance.  For each sensor, take the mean over a particular week,
 #   then plot the variance.
-# How correlated are the data from different sensors?
+# How correlated are the data from different sensors at the same time?
+
+# What commercial questions would I want to answer?
+# * How reliable is solar power? How much does solar electricity output fluctuate?
+# * What is the potential scope for rooftop solar? (How much capacity? Comparison with demand?)
+# * How large is that for household needs?  What of industrial/commerical purposes?
+# * How much of total energy usage is electricity?
 
 
 
