2d852ab0f6bd4b2996a47861610c8bc606bdaf46
jmackrory <jonathan.mackrory@gmail.com>
2017-10-19 15:44:11 -0700

    added function to read in a single time series.

--- get_weather_data.py
+++ get_weather_data.py
@@ -5,10 +5,9 @@ import pandas as pd
 import numpy as np
 import ftplib 
 
-
-#Get API keys from JSON file
-with open('keys.json') as key_file:
-    keys=json.load(key_file)
+# #Get API keys from JSON file
+# with open('keys.json') as key_file:
+#     keys=json.load(key_file)
 #NCDC's call to website.
 
 biggest_cities=[
@@ -63,27 +62,6 @@ biggest_cities=[
     ['WI','Milwaukee','Madison','Green Bay'],
     ['WY','Cheyenne','Casper','Laramie']];
 
-# #read in DataFrame of largest 3 cities in each US state. (list from Wikipedia)
-# def make_bigcity_list(biggest_city,depth=3):
-#     names=['State','L1','L2','L3']
-#     nrows = len(biggest_cities);
-#     biggest_city_list=list()
-#     #now make a list of "city,state" pairs
-#     for i in range(0,nrows):
-#         for j in range(1,depth+1):
-#             city_str=biggest_cities[i][j]+', '+biggest_cities[i][0]
-#             biggest_city_list.append(city_str)
-
-#     return biggest_city_list    
-# city_list=make_bigcity_list(biggest_cities,depth=3)
-
-#read in list of airports
-airport_df = pd.read_csv('data/airports.dat',skiprows=1,na_values='\\N')
-print(len(airport_df))
-#only keep US airports, and name,city, and ICAO codes
-msk2=airport_df['Country']=='United States'
-airport_df=airport_df[msk2][['Name','City','ICAO']]
-print(len(airport_df))
 
 # #now make a dict of city names, and station locations.
 # #Find allowed ID number corresponding to largest cities.
@@ -106,10 +84,21 @@ def get_airport_code(dataframe,city_list,depth=3):
     aircode_df=aircode_df.rename(columns={'ICAO':'CALL'})
     return aircode_df
 
+#read in list of airports
+airport_df = pd.read_csv('data/airports.dat',skiprows=1,na_values='\\N')
+print(len(airport_df))
+#only keep US airports, and name,city, and ICAO codes
+msk2=airport_df['Country']=='United States'
+airport_df=airport_df[msk2][['Name','City','ICAO']]
+print(len(airport_df))
+
 airport_codes=get_airport_code(airport_df,biggest_cities)
 
 #now compare with stations from ISD database.
 isd_name_df=pd.read_fwf('data/ISD/isd-history.txt',skiprows=20)
+#also only keep airports still operational in time period.
+msk = isd_name_df['END']>20150000
+isd_name_df=isd_name_df[msk]
 isd_name_df=isd_name_df[['USAF','WBAN','CALL']]
 
 #merge together.
@@ -120,107 +109,84 @@ msk1 = airport_codes2['USAF']!=999999
 msk2 = airport_codes2['WBAN']!=99999
 airport_codes2=airport_codes2[msk1&msk2]
 
+airport_codes2['USAF']=airport_codes2['USAF'].astype(int)
+airport_codes2['WBAN']=airport_codes2['WBAN'].astype(int)
+
 #now download the data from NOAA:
-
-# def noaa_ftp():
-#     ftp=ftplib.FTP('ftp.ncdc.noaa.gov')
-#     ftp.cwd('pub/data/noaa/isd-lite')
-#     return ftp
-
-
-# def download_city_data(ftp,USAF,WBAN,yearstr):
-#     ftp.cwd(str(yearstr))
-
-#     cmd_name='RETR '+USAF+'-'+WBAN+'-'+yearstr+'.gz'
-#     ftp.retrbinary(cmd_name)
-
-def wget_data(USAF,WBAN,yearstr):
+def wget_data(USAF,WBAN,yearstr,city,airport):
     base_url='ftp://ftp.ncdc.noaa.gov/pub/data/noaa/isd-lite'
-    url=base_url+'/'+yearstr+'/'+USAF+'-'+WBAN+'-'yearstr'.gz'
-    wget.download(url,out='data/ISD')
-# ftp=noaa_ftp()
-# cd data/isd_data
+    #put in some padding {:0>5} for shorter codes.
+    url=base_url+"/{0}/{1}-{2:0>5}-{0}.gz".format(yearstr,str(USAF),str(WBAN))
+    try:
+        wget.download(url,out='data/ISD')
+    except:
+        print('could not download data from city:',city,airport)
 
+#download weather data for all of the airports specified in aircode
+def get_all_data(aircode,years=['2015','2016','2017']):
+    for yearstr in years:
+        for i in range(len(aircode)):
+            ap = aircode.iloc[i]
+            usaf=ap['USAF']
+            wban=ap['WBAN']
+            city=ap['City']
+            airport=ap['Name']
+            wget_data(usaf,wban,yearstr,city,airport)
+    return None
+##actually download that data
+#get_all_data(airport_codes2)
+
+#now read it in, convert to time-series.
+
+
+
+def convert_isd_to_df(filename,city,state):
+    """
+    convert_to_df(filename)
     
-# #Find all cities with Local Climatological Data.
-# #This dataset has hourly temperature/preciptation measurements.
-# def get_lcd_data_locations():
-#     count = 0
-#     Nretrieve=1000
-#     keep_going=True
-#     lcd_frame=pd.DataFrame()
-#     #loop through all available results.
-#     #took around 10 sec.
-#     while (keep_going==True):
-#         print('count=',count)
-#         offset=count*Nretrieve
-#         url1=baseurl+'locations?datasetid=LCD&limit=1000&offset={}'.format(offset,)
-#         response1=requests.get(url1,headers=h1)
-#         lcd_list=eval(response1.text)
-#         df=pd.DataFrame.from_dict(lcd_list["results"])
-#         df_length=df.shape[0]
-#         lcd_frame=lcd_frame.append(df)
-#         count+=1
-#         if (df_length != Nretrieve):
-#             print('Hit the end:',df_length,Nretrieve)
-#             keep_going=False
-#             lcd_frame.index=np.arange(0,lcd_frame.shape[0])
-#     return lcd_frame
-# #dump that dataframe to file.
-# #lcd_frame=get_lcd_data_locations()
-# #lcd_frame.to_json('data/lcd_city_list.json',orient='records',lines=True)
-# #fgfd
-# ## Read in from previously read in 
-# #lcd_frame=pd.read_json('data/lcd_city_list.json',orient='records',lines=True)
+    Read in a automated weather stations data from file.
+    Data is space separated columns, with format given in
+    "isd-lite-format.txt".
+    Converts to pandas dataframe using date/time columns as DateTimeIndex.
+    with other variables 
+    """
 
-# #now make a dict of city names, and station locations.
-# #Find allowed ID number corresponding to largest cities.
-# #Note not all of these have entries.  
-def get_ids(dataframe,loc_list):
-    loc_dict=dict()
-    for city in loc_list:
-        msk=dataframe['name'].str.contains(city)
-        #uses fact that in returned list that the first entries in this list
-        # are all of the big entries for cities with multiple zip codes.
-        if (sum(msk)>0):
-            id=dataframe['id'][msk]
-        else:
-            print('couldn\'t find city:'+city)
-            id=None
-        loc_dict[city]=id
-    return loc_dict
+    """Format info:
+    1: Year
+    2: Month
+    3: Day
+    4: Hour 
+    5: Temperature (x10) in celcius
+    6: Dew point temperature (x10) in celcius
+    7: Sea level pressure (x10 in hectopascals)
+    8: Wind direction (degrees from north)
+    9: Wind speed 
+    10: Cloud Coverage (categorical)
+    11: Precipitation for One Hour (x10, in mm)
+    12: Precipitation total for Six hours (x10 in mm)
 
+    All missing values are -9999.
+    """
+    #use fixed width format to read in (isd-lite-format has data format)
+    col_names=['year','month','day','hour',
+               'Temp','DewTemp','Pressure',
+               'WindDir','WindSpeed','CloudCover',
+               'Precip-1hr','Precip-6hr']
+    df=pd.read_fwf(filename,compression='gzip',
+                   na_values='-9999',names=col_names)
+                   #parse_dates=[0,1,2,3],infer_datetime_format=True,
+    times=pd.to_datetime({'year':df['year'],
+                          'month':df['month'],
+                          'day':df['day'],
+                          'hour':df['hour']})
+    df.index=pd.DatetimeIndex(times)
 
-# #works?
-# url0='https://www.ncdc.noaa.gov/cdo-web/api/v2/'\
-#     +'data?datasetid=GHCND&locationid=ZIP:97218&startdate=2016-05-01&enddate=2016-05-01'
+    #df=pd.read_fwf(filename,compression='gzip',na_values='-9999')
+    
+    return df
 
-# def make_url(dataset=None,loc_id=None,start=None,end=None):
-#     url='data?datasetid={}&locationid={}&startdate={}&enddate={}'.format(dataset,loc_id,start,end)
-#     return url
+fn = 'data/ISD/702650-26407-2015.gz'
+city='Blah'
+state='MEH'
 
-
-# #taken from location page.
-# def call_website(url_ext):
-#     baseurl='https://www.ncdc.noaa.gov/cdo-web/api/v2/'
-#     url=baseurl+url_ext
-#     print('new url:\n',url)
-#     response2=requests.get(url,headers=h1)
-#     # #gets a json response
-#     try:
-#         data=eval(response2.text)
-#     except:
-#         print('cant read first response')
-#         #try old one:
-#     # print('old url:\n',url0)
-#     # response2=requests.get(url0,headers=h1)
-#     # # #gets a json response
-#     # try:
-#     #     data=eval(response2.text)
-#     # except:
-#     #     print('cant read second response')
-#     return data
-
-# #download data set for specified date range.
-# #put dataset into SQL table.
-
+df=convert_isd_to_df(fn,city,state)
