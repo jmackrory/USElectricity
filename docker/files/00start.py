e379224d907f6210e2027830c770ffb69967100d
Jonathan Mackrory <jmackrory@users.noreply.github.com>
2023-05-19 17:50:38 -0700

    moved library files.  currently borked with editable install not allowing imports

--- docker/files/00start.py
+++ docker/files/00start.py
@@ -1,13 +1,13 @@
 import numpy as np
-import tensorflow as tf
 import matplotlib.pyplot as plt
 import pandas as pd
 import sys
 
 from IPython import get_ipython
-ipython = get_ipython()
-ipython.magic('load_ext autoreload')
-ipython.magic('autoreload 2')
 
-if '/tf' not in sys.path:
-    sys.path.append('/tf/')
+ipython = get_ipython()
+ipython.magic("load_ext autoreload")
+ipython.magic("autoreload 2")
+
+if "/tf" not in sys.path:
+    sys.path.append("/tf/")
